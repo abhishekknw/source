8b9c1c71b327f361fff56d1375f2d4c4fa826ec3
khushboo9293 <khushboo9293@gmail.com>
2016-01-07 09:43:10 +0530

    initial commit

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.mysql',
        'HOST': '127.0.0.1',
        'USER': 'root',
-       'PASSWORD': 'mysql',
+       'PASSWORD': 'khush123',
     }
 }
 
