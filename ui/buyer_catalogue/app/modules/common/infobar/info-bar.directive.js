0f87331e61f1f99751645cd87e214218333a456e
Arunabh Trivedi <arunabh.trivedi@gmail.com>
2016-01-25 17:43:27 +0530

    adding campaign bar

---
+++ ui/buyer_catalogue/app/modules/common/infobar/info-bar.directive.js
@@ -0,0 +1,10 @@
+angular.module('machadaloCommon')
+.directive('infoBar', function() {
+  return {
+  restrict: 'E',
+  templateUrl: 'modules/common/infobar/info-bar.tmpl.html',
+  link: function(scope, element, attrs) {
+    scope.campaignname = attrs.campaignname;
+    scope.societyname = attrs.societyname;
+  }};
+});
