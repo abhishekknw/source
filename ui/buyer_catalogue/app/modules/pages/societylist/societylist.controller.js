d2bf92c3ab9dc2f1c3fe355b48ccacf9b77aee57
vidhi <vidhi@machadalo.com>
2016-02-10 17:21:11 +0530

    changes for society list page

--- ui/buyer_catalogue/app/modules/pages/societylist/societylist.controller.js
+++ ui/buyer_catalogue/app/modules/pages/societylist/societylist.controller.js
@@ -1,7 +1,8 @@
 angular.module('machadaloPages')
 .controller('SocietyListCtrl',
     ['$scope', '$rootScope', '$window', '$location',
-    function ($scope, $rootScope, $window, $location, $http) {
+    function ($scope, $rootScope, $window, $location, $http, societyListService) {
+      //Start: for filter functionality
       $scope.getLocation = function(val) {
          return $http.get('https://maps.googleapis.com/maps/api/geocode/json', {
            params: {
@@ -14,5 +15,20 @@ angular.module('machadaloPages')
              return item.formatted_address;
            });
          });
-       };
+       };// End: filter functionality
+
+  $scope.model = {};
+
+  var dummyData = [
+   {
+       "societyname":"23C_Tower1_1401",
+       "location":"next to baskin"
+   },
+   {
+       "societyname":"23C_Tower1_1402",
+       "location":"next to theo"
+   }
+  ];
+  $scope.model = dummyData;
+
     }]);
