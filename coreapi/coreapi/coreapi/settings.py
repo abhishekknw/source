ffebf83d577390b3e03ca37dd3edc293ab1a1d0f
khushboo9293 <khushboo9293@gmail.com>
2016-03-16 12:58:59 +0530

    changes

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -85,14 +85,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'default1': {
-       'NAME': 'machadalo',
+    'default': {
+       'NAME': 'machadalo_dev',
        'ENGINE': 'django.db.backends.mysql',
        'HOST': '127.0.0.1',
        'USER': 'root',
        'PASSWORD': 'khush123',
     },
-    'default': {
+    'default1': {
         'NAME':'testvidhi',
          'ENGINE': 'django.db.backends.mysql',
          'HOST': '127.0.0.1',
