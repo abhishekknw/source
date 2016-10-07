0c2624f1fc564bbce556b0f1a9eba9329024b203
yogi07 <yogesh.mhetre@machadalo.com>
2016-10-07 12:25:33 +0530

    added fields content_type and object_id in FlatType model

--- coreapi/coreapi/v0/models.py
+++ coreapi/coreapi/v0/models.py
@@ -336,6 +336,10 @@ class FlatType(models.Model):
     size_builtup_area = models.FloatField(db_column='SIZE_BUILTUP_AREA', blank=True, null=True)  # Field name made lowercase.
     flat_rent = models.IntegerField(db_column='FLAT_RENT', blank=True, null=True)  # Field name made lowercase.
     average_rent_per_sqft = models.FloatField(db_column='AVERAGE_RENT_PER_SQFT', blank=True, null=True)  # Field name made lowercase.
+    content_type = models.ForeignKey(ContentType,default=None, null=True)
+    object_id = models.CharField(max_length=12, null=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    objects = managers.GetInventoryObjectManager()
 
     class Meta:
         db_table = 'flat_type'
