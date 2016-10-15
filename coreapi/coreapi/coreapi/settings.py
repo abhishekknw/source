f3785f30bcdb5b6d31a45c3a19228f78d27ebef1
yogi07 <yogesh.mhetre@machadalo.com>
2016-10-15 15:52:37 +0530

    changes while pulling code

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'HOST': 'localhost',
          'USER': 'root',
-         'PASSWORD': 'root',
+         'PASSWORD': 'yo@123',
       }
 
     }
@@ -120,25 +120,25 @@ STATIC_URL = '/static/'
 
 # settings for Django Rest Framework
 
-'''REST_FRAMEWORK = {
-    # other settings...
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
-    'DEFAULT_PERMISSION_CLASSES': [],
-}'''
+# '''REST_FRAMEWORK = {
+#     # other settings...
+#     'DEFAULT_AUTHENTICATION_CLASSES': [],
+#     'DEFAULT_PERMISSION_CLASSES': [],
+# }'''
 
 
-# REST_FRAMEWORK = {
-#      'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#         ),
-#      'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#         'rest_jwt.authentication.JSONWebTokenAuthentication',
-#         ),
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 5
-# }
+REST_FRAMEWORK = {
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        ),
+     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_jwt.authentication.JSONWebTokenAuthentication',
+        ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
 
 
 # CORS headers
