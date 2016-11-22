0cfcad08c7b98807f91127745f23896e444709ad
vidhi <vidhi@machadalo.com>
2016-11-22 13:23:55 +0530

    auto migrations

---
+++ coreapi/coreapi/v0/migrations/0053_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('v0', '0046_merge'),
+        ('v0', '0052_auto_20161116_1318'),
+        ('v0', '0052_auto_20161116_1031'),
+    ]
+
+    operations = [
+    ]
