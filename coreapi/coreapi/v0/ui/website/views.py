5cf8359bd57478b289027cecf6ed0d95ebf96b3e
sherelock <nikhil.singh@machadalo.com>
2016-12-12 12:24:28 +0530

    @NewFeature  Added 'suppliers_meta' field in response - Earlier the API get_spaces() of proposal view set was not returning suppliers_meta information which contained filters inforamtion which was necessary to show on map - view page. So reused a function called add_filters() to include suppliers_meta information in the response.

--- coreapi/coreapi/v0/ui/website/views.py
+++ coreapi/coreapi/v0/ui/website/views.py
@@ -2819,6 +2819,10 @@ class ProposalViewSet(viewsets.ViewSet):
                 {
                    suppliers: { RS: [], CP: [] } ,
                    center: { ...   codes: [] }
+                   suppliers_meta: {
+                                     'RS': { 'inventory_type_selected' : [ 'PO', 'POST', 'ST' ]  },
+                                     'CP': { 'inventory_type_selected':  ['ST']
+                   }
                 }
            ]
 
@@ -2833,8 +2837,8 @@ class ProposalViewSet(viewsets.ViewSet):
           description:  center_id
         - name: radius
           description: radius
-        - name: supplier_codes
-          description:  array of codes like RS, CP, etc
+        - name: latitude
+        - name: longitude
         """
         class_name = self.__class__.__name__
         try:
