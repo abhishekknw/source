2e06cc8ed571c0742545c694ef19d4473e8cae77
yogi07 <yogesh.mhetre@machadalo.com>
2016-12-20 19:07:29 +0530

    @BugFix - when any business is selected and coming back to business page the business was again to original state so fixed that by using sessionStorage which will store data , getStoredData function is written to store data

--- ui/buyer_catalogue/app/modules/pages/manageCampaign/create/create-campaign.controller.js
+++ ui/buyer_catalogue/app/modules/pages/manageCampaign/create/create-campaign.controller.js
@@ -30,7 +30,7 @@ angular.module('machadaloPages')
         $scope.dt = new Date(year, month, day);
       };
 
-      $scope.sel_account_id = null;
+      // $scope.sel_account_id = null;
       $scope.dateOptions = {
         formatYear: 'yy',
         startingDay: 1
@@ -57,6 +57,28 @@ angular.module('machadaloPages')
       var contactCopy = angular.copy($scope.contact);
       $scope.model.business.contacts = [$scope.contact];
 
+      // Start: for persisting values after refresh or back from other pages
+      $scope.getStoredData = function(){
+        if($window.sessionStorage.business != null){
+          $scope.model.business = JSON.parse($window.sessionStorage.business);
+          if($window.sessionStorage.accounts != null){
+            $scope.model.accounts = JSON.parse($window.sessionStorage.accounts);
+            if($window.sessionStorage.sel_account_index >= 0){
+              $scope.sel_account_id = $scope.model.accounts[$window.sessionStorage.sel_account_index].account_id;
+            }
+          }
+          if($window.sessionStorage.account_proposals != null)
+            $scope.account_proposals = JSON.parse($window.sessionStorage.account_proposals);
+          $scope.choice = "selected";
+        }else {
+          $scope.model.business = null;
+          $scope.model.accounts = null;
+          $scope.account_proposals = null;
+        }
+      }
+      $scope.getStoredData();
+      // End: for persisting values after refresh or back from other pages
+
       pagesService.loadBusinessTypes()
       .success(function (response){
           $scope.busTypes = response;
@@ -67,15 +89,27 @@ angular.module('machadaloPages')
               $scope.model.business = response.business;
               $scope.model.accounts = response.accounts;
               $rootScope.business_id = response.business.business_id;
-              $window.localStorage.business_id = response.business.business_id;
+              $window.sessionStorage.business_id = response.business.business_id;
               $rootScope.business_name = response.business.name;
-              $window.localStorage.business_id = response.business.name;
+              $window.sessionStorage.business_id = response.business.name;
               $scope.model.business.business_type_id = $scope.model.business.type_name.id.toString();
               $scope.getSubTypes();
               $scope.model.business.sub_type_id = $scope.model.business.sub_type.id.toString();
               $scope.choice = "selected";
               // pagesService.setBusinessObject($scope.model.business);
-              $window.localStorage.business = JSON.stringify($scope.model.business);
+              //Start: added to persit data after refresh
+              $window.sessionStorage.business = JSON.stringify($scope.model.business);
+              if($scope.model.accounts.length != 0){
+                $window.sessionStorage.accounts = JSON.stringify($scope.model.accounts);
+              }else{
+                $window.sessionStorage.accounts = null;
+              }
+              $window.sessionStorage.account_proposals = null;
+              $scope.account_proposals = null;
+              $window.sessionStorage.sel_account_index = -1;
+              $scope.sel_account_id = null;
+              $scope.error = false;
+              //End: added to persit data after refresh
          });
       };
 
@@ -111,6 +145,8 @@ angular.module('machadaloPages')
       };
 
     	$scope.getAllBusinesses = function() {
+        $window.sessionStorage.account_proposals = null;
+        $window.sessionStorage.sel_account_index = null;
         $scope.bsSelect = undefined;
 	    	pagesService.getAllBusinesses()
 	    	.success(function (response, status) {
@@ -153,15 +189,22 @@ angular.module('machadaloPages')
               $location.path("/manageCampaign/createAccount");
       };
 
-      $scope.getProposals = function(sel_account_id){
+      $scope.getProposals = function(sel_account_id,index){
           $scope.error = false;
           // pass account_id of selected account radio button
           $scope.sel_account_id = sel_account_id;
-          $rootScope.account_id = sel_account_id;
-          $window.localStorage.account_id = sel_account_id;
+          // $rootScope.account_id = sel_account_id;
+
+          //start : added to persist data after refresh
+          $window.sessionStorage.sel_account_index = index;
+          $window.sessionStorage.account_id = sel_account_id;
+
+          //start : added to persist data after refresh
+
           pagesService.getAccountProposal(sel_account_id)
           .success(function(response, status){
               $scope.account_proposals = response.data;
+              $window.sessionStorage.account_proposals = JSON.stringify($scope.account_proposals);
           })
           .error(function(response, status){
               if(typeof(response) == typeof([]))
@@ -177,25 +220,24 @@ angular.module('machadaloPages')
         }
         else{
           pagesService.setProposalAccountId(sel_account_id);
-          $window.localStorage.proposal_id = 0;
+          $window.sessionStorage.proposal_id = 0;
           $location.path('/'+sel_account_id + '/createproposal');
         }
       }
 
       $scope.showProposalDetails = function(proposal_id){
-        $window.localStorage.parentProposal = true;
-        $window.localStorage.proposal_id = proposal_id;
+        $window.sessionStorage.parentProposal = true;
+        $window.sessionStorage.proposal_id = proposal_id;
         $location.path('/' + proposal_id + '/showcurrentproposal');
       }
 
-      $scope.showHistory = function(proposalId){$window.localStorage.proposal_id = proposalId;
-        $window.localStorage.proposal_id = proposalId;
+      $scope.showHistory = function(proposalId){$window.sessionStorage.proposal_id = proposalId;
+        $window.sessionStorage.proposal_id = proposalId;
         $location.path('/' + proposalId + '/showproposalhistory');
       }
     	$scope.create = function() {
             pagesService.createBusinessCampaign($scope.model)
             .success(function (response, status) {
-
             var sub_type_id = $scope.model.business.sub_type_id;
             var type_id = $scope.model.business.business_type_id;
             // response = JSON.parse(response);
@@ -211,7 +253,7 @@ angular.module('machadaloPages')
             if (status == '200'){
               $scope.choice = "selected";
               pagesService.setBusinessObject($scope.model.business);
-              $window.localStorage.business = JSON.stringify($scope.model.business);
+              $window.sessionStorage.business = JSON.stringify($scope.model.business);
             }
         }).error(function(response, status){
              if (typeof response != 'number'){
