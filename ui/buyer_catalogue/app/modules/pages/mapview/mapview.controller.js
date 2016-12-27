864fb220d730afa9659b82e4ed8e78cf83214359
yogi07 <yogesh.mhetre@machadalo.com>
2016-12-27 11:56:37 +0530

    @BugFix mapview - after edit proposal the getspaces were getting called so fixed that to call shortlisted_spaces

--- ui/buyer_catalogue/app/modules/pages/mapview/mapview.controller.js
+++ ui/buyer_catalogue/app/modules/pages/mapview/mapview.controller.js
@@ -477,7 +477,8 @@ $scope.options = { scrollwheel: false, mapTypeControl: true,
 // This service gets all the spaces according to center specification like society_allowed
           //Start: adding code to call shortlisted_spaces api if the proposal data is already saved
           $scope.proposal_id_temp = $stateParams.proposal_id;
-          if($window.sessionStorage.isSavedProposal == true){
+          console.log($window.sessionStorage.isSavedProposal);
+          if($window.sessionStorage.isSavedProposal == 'true'){
             mapViewService.getShortlistedSuppliers($scope.proposal_id_temp)
               .success(function(response, status){
                 console.log(response);
@@ -486,16 +487,20 @@ $scope.options = { scrollwheel: false, mapTypeControl: true,
                 $scope.center_data = $.map(response.data, function(value, index){
                   return [value];
                 });
-                console.log($scope.center_data);
+                var flag;
+                for(var i=0;i<$scope.center_data.length;i++){
+                  if($scope.center_data[i].suppliers_meta != null){
+                    flag = true;
+                  }
+                }
+                if(flag == true){
+                  checkSavedFilters();
+                }
                 $scope.addSupplierFilters($scope.center_data);
                 $scope.current_center = $scope.center_data[0];
                 $scope.current_center_index = 0;
                 $scope.current_center_id = $scope.current_center.center.id;
                 $scope.old_data = angular.copy($scope.center_data);
-
-                // if($scope.current_center.suppliers_meta != null){
-                //   checkExportedFilters($scope.current_center);
-                // }
 
                 mapViewBasicSummary();
                 suppliersData();
@@ -555,7 +560,13 @@ $scope.options = { scrollwheel: false, mapTypeControl: true,
                 //loading icon
                 $scope.loadIcon2 = response;
                 //Start: code added if proposal is already created or exported, and user wants to edit that proposal
-                if($scope.current_center.suppliers_meta != null){
+                var flag;
+                for(var i=0;i<$scope.center_data.length;i++){
+                  if($scope.center_data[i].suppliers_meta != null){
+                    flag = true;
+                  }
+                }
+                if(flag == true){
                   checkSavedFilters();
                 }
                 //End: code added if proposal is already created or exported and user wants to edit that proposal
@@ -651,7 +662,6 @@ $scope.options = { scrollwheel: false, mapTypeControl: true,
       for(var i=0; i<$scope.center_data.length;i++){
           if($scope.center_data[i].suppliers_meta['RS'] != null){
             var filter_types = Object.keys($scope.center_data[i].suppliers_meta['RS']);
-            console.log(filter_types);
             for(var j=0;j<filter_types.length;j++){
               if(filter_types[j]=='inventory_type_selected'){
                 selectFilters($scope.center_data[i].suppliers_meta['RS'][filter_types[j]],$scope.RS_filters['inventory']);
@@ -662,6 +672,7 @@ $scope.options = { scrollwheel: false, mapTypeControl: true,
       }
     }
     var selectFilters = function(saved_filter_type,current_filter_type){
+      console.log("hi");
       for(var i=0;i<saved_filter_type.length;i++){
         for(var j=0;j<current_filter_type.length;j++){
           if(saved_filter_type[i]==current_filter_type[j].code)
