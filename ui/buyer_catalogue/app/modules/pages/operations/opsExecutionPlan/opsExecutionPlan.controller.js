84e67f118bb10b5d7457738e14699fc2662816b9
yogi07 <yogesh.mhetre@machadalo.com>
2017-02-07 11:16:13 +0530

    @BugFix OpsExecution - integrated with backend and displayed data in table

--- ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlan.controller.js
+++ ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlan.controller.js
@@ -4,6 +4,7 @@ angular.module('catalogueApp')
     function ($scope, $rootScope, $window, $location, opsExecutionPlanService, $stateParams) {
       $scope.campaign_id = $stateParams.proposal_id;
       $scope.headings = [
+        {header : 'Supplier Id'},
         {header : 'Inventory Id'},
         {header : 'Inventory Type'},
         {header : 'Image'},
@@ -12,6 +13,41 @@ angular.module('catalogueApp')
         {header : 'Comments'},
         {header : 'ReAssign'},
       ];
-      
-     
+      $scope.supplier_headings = [
+        {header : 'Supplier Id'},
+        {header : 'Supplier Name'},
+        {header : 'Area'},
+        {header : 'SubArea'},
+         {header : 'PinCode'},
+      ];
+
+      opsExecutionPlanService.getOpsExecutionImageDetails($scope.campaign_id)
+      	.success(function(response, status){
+          console.log(response);
+      		$scope.campaignData = response.data;
+              $scope.loading = response;
+      	})
+      	.error(function(response, status){
+      		console.log("error occured", status);
+      	});
+      $scope.setImageUrl = function(image_path){
+        console.log(image_path);
+        $scope.image_url = "http://androidtokyo.s3.amazonaws.com/" + image_path;
+      }
+      $scope.getSupplierDetails = function(supplier){
+        console.log(supplier);
+        var supplierId = supplier.inventory_details.shortlisted_supplier.object_id;
+        var contentType = supplier.inventory_details.shortlisted_supplier.content_type;
+        opsExecutionPlanService.getSuppierDetails(supplierId,contentType)
+        	.success(function(response, status){
+            $scope.supplierData = response.data;
+            // console.log(response);
+      	   })
+        	.error(function(response, status){
+        		console.log("error occured", status);
+        	});
+
+      }
+
+
 }]);
--- ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlanService.js
+++ ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlanService.js
@@ -6,7 +6,16 @@ angular.module('catalogueApp')
    var url_base = 'v0/ui/website/';
    var opsExecutionPlanService = {};
 
+   opsExecutionPlanService.getOpsExecutionImageDetails = function(proposal_id){
+        var url = url_base + "inventory-activity-image/" + "?proposal_id=" + proposal_id;
+     return machadaloHttp.get(url);
+   }
 
-  
+   opsExecutionPlanService.getSuppierDetails = function(supplier_id,content_type){
+     var url = url_base + "supplier-basic-details/" + "?supplier_id=" + supplier_id  + "&content_type=" + content_type;
+     return machadaloHttp.get(url);
+   }
+
+
    return opsExecutionPlanService;
 }]);
