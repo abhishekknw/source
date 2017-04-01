15718425c5b4a2444e7300dbecf75b4d72ca624c
sherelock <nikhil.singh@machadalo.com>
2017-04-01 16:16:15 +0530

    @EditFearture: Modified society serializer to fetch only desired fields from table.

--- coreapi/coreapi/v0/serializers.py
+++ coreapi/coreapi/v0/serializers.py
@@ -219,6 +219,12 @@ class SportsInfraSerializer(ModelSerializer):
 class SupplierTypeSocietySerializer(ModelSerializer):
     class Meta:
         model = SupplierTypeSociety
+        fields = ('supplier_id', 'society_name', 'society_address1', 'society_address2', 'society_zip', 'society_city',
+                  'society_state', 'society_longitude', 'society_locality', 'society_subarea', 'society_latitude', 'society_location_type',
+                  'society_type_quality', 'society_type_quantity', 'flat_count', 'flat_avg_rental_persqft', 'flat_sale_cost_persqft',
+                  'tower_count', 'payment_details_available', 'age_of_society',
+                  )
+
 
 
 class SupplierTypeCorporateSerializer(ModelSerializer):
