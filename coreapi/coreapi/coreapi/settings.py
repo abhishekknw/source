297be104dcd2d9e3636f37d07ecaf0f63b7c74e1
kaynaat <nix.harry81@gmail.com>
2017-04-08 14:25:17 +0530

    configured settings.py filw with changes for RDS

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -84,21 +84,33 @@ WSGI_APPLICATION = 'coreapi.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-DATABASES = {
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-
-    'default': {
-        'NAME':'mdtestnew',
-         'ENGINE': 'django.db.backends.mysql',
-         'HOST': 'mdtest.cncgdhp3beic.ap-southeast-1.rds.amazonaws.com',
-         'USER': 'mdtest',
-         'PASSWORD': 'mdtestmachadalo',
-      }
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'sqlite': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+        'default': {
+            'NAME': 'mdtest',
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': 'mdtest.cncgdhp3beic.ap-southeast-1.rds.amazonaws.com',
+            'USER': 'mdtest',
+            'PASSWORD': 'mdtestmachadalo',
+        }
 
     }
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
