6b38cfe56500d473b4ff13333352e0cff8fb8722
yogesh <yogesh.mhetre@machadalo.com>
2017-09-08 15:37:14 +0530

    @EditFeature opsExecution - replaced supplier id with supplier name

--- ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlan.controller.js
+++ ui/buyer_catalogue/app/modules/pages/operations/opsExecutionPlan/opsExecutionPlan.controller.js
@@ -11,8 +11,8 @@ angular.module('catalogueApp')
       }
       $scope.headings = [
         {header : 'Index'},
-        {header : 'Supplier Id'},
-        {header : 'Inventory Id'},
+        {header : 'Supplier Name'},
+        // {header : 'Inventory Id'},
         {header : 'Inventory Type'},
         {header : 'Image'},
          {header : 'Activity Name'},
@@ -59,6 +59,7 @@ angular.module('catalogueApp')
       var getOpsExecutionImageDetails = function(){
         opsExecutionPlanService.getOpsExecutionImageDetails($scope.campaign_id)
         	.then(function onSuccess(response){
+            console.log(response);
         		$scope.campaignData = response.data.data;
             createList();
             if($scope.campaignData.length == 0)
@@ -108,6 +109,7 @@ angular.module('catalogueApp')
                       var data = angular.copy(campaignDataStruct);
                       data.id = assignId;
                       data.supplier_id = $scope.campaignData.shortlisted_suppliers[spaceId].supplier_id;
+                      data.supplier_name = $scope.campaignData.shortlisted_suppliers[spaceId].supplier_detail.name;
                       data.inv_id = $scope.campaignData.shortlisted_inventories[invId].inventory_id;
                       data.inv_type = $scope.campaignData.shortlisted_inventories[invId].inventory_name;
                       data.act_name = $scope.campaignData.inventory_activities[actId].activity_type;
