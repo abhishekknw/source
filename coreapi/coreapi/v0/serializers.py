63732319d798771d01f7f7175def55b2a9150dd1
yogesh <yogesh.mhetre@machadalo.com>
2018-08-02 15:52:11 +0530

    @Bugfix create suppier - supplier code for society was not getting saved

--- coreapi/coreapi/v0/serializers.py
+++ coreapi/coreapi/v0/serializers.py
@@ -248,7 +248,7 @@ class SportsInfraSerializer(ModelSerializer):
 class SupplierTypeSocietySerializer(ModelSerializer):
     class Meta:
         model = SupplierTypeSociety
-        fields = ('supplier_id', 'society_name', 'society_address1', 'society_address2', 'society_zip', 'society_city',
+        fields = ('supplier_id', 'society_name', 'society_address1', 'society_address2', 'society_zip', 'society_city','supplier_code',
                   'society_state', 'society_longitude', 'society_locality', 'society_subarea', 'society_latitude', 'society_location_type',
                   'society_type_quality', 'society_type_quantity', 'flat_count', 'flat_avg_rental_persqft', 'flat_sale_cost_persqft',
                   'tower_count', 'payment_details_available', 'age_of_society','total_tenant_flat_count','landmark','feedback',
