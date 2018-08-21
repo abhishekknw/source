4e618b569943675cec5a268967edaf2ac1a38076
Kushagra <kushagra.1986@gmail.com>
2018-08-21 19:57:45 +0530

    get checklist data function created

--- coreapi/coreapi/v0/ui/checklists/urls.py
+++ coreapi/coreapi/v0/ui/checklists/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from views import Test, CreateChecklistTemplate, ChecklistEntry, GetCampaignChecklists, GetSupplierChecklists
+from views import (Test, CreateChecklistTemplate, ChecklistEntry, GetCampaignChecklists,
+    GetSupplierChecklists, GetChecklistData)
 
 urlpatterns = [
     url(r'^test/$', Test.as_view()),
@@ -7,4 +8,6 @@ urlpatterns = [
     url(r'^(?P<checklist_id>[A-Z_a-z0-9]+)/enter_data$', ChecklistEntry.as_view()),
     url(r'^(?P<campaign_id>[A-Z_a-z0-9]+)/list_campaign_checklists$', GetCampaignChecklists.as_view()),
     url(r'^(?P<campaign_id>[A-Z_a-z0-9]+)/list_supplier_checklists/(?P<supplier_id>[A-Z_a-z0-9]+)$', GetSupplierChecklists.as_view()),
+    url(r'^(?P<checklist_id>[A-Z_a-z0-9]+)/get_data', GetChecklistData.as_view()),
+
 ]
--- coreapi/coreapi/v0/ui/checklists/views.py
+++ coreapi/coreapi/v0/ui/checklists/views.py
@@ -73,7 +73,8 @@ class ChecklistEntry(APIView):
                 "item_id": item["item_id"],
                 "item_value": item["item_value"],
                 "status": "active",
-                "supplier_id": supplier_id
+                "supplier_id": supplier_id,
+                "entry_id": entry_id
             }))
             checklist_entry_list.append(row)
             row.save()
@@ -102,4 +103,27 @@ class GetSupplierChecklists(APIView):
             checklist_dict.append(list_item)
         return ui_utils.handle_response({}, data=checklist_dict, success=True)
 
-
+class GetChecklistData(APIView):
+    # viewing a particular checklist
+    @staticmethod
+    def get(request,checklist_id):
+        checklist_info = Checklist.objects.get(id=checklist_id)
+        last_entry_id = checklist_info.last_entry_id
+        checklist_columns = ChecklistColumns.objects.filter(checklist_id=checklist_id)
+        checklist_data = ChecklistData.objects.filter(checklist_id=checklist_id)
+        checklist_rows = []
+        for i in range(1, last_entry_id+1):
+            entry_data = checklist_data.filter(entry_id = i)
+           # if entry_data is not None:
+            current_row = {}
+            for keys in checklist_columns:
+                key_name = keys.key_name
+                item_id = keys.item_id
+                key_query = entry_data.filter(item_id=item_id).first()
+                if (not key_query):
+                    continue
+                key_value = key_query.item_value
+                current_row[key_name] = key_value
+            if current_row != {}:
+                checklist_rows.append(current_row)
+        return ui_utils.handle_response({}, data=checklist_rows, success=True)
