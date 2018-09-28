905d3df54466d63355864898561d67a9d036f4b9
Kshitij Mittal <kshitij.mittal01@gmail.com>
2018-09-28 16:38:11 +0530

    api to add permissions manually

--- coreapi/coreapi/v0/ui/permissions/views.py
+++ coreapi/coreapi/v0/ui/permissions/views.py
@@ -2,13 +2,14 @@ from rest_framework import viewsets
 from serializers import (ObjectLevelPermissionSerializer, GeneralUserPermissionSerializer, RoleSerializer)
 from models import ObjectLevelPermission, GeneralUserPermission, Role, RoleHierarchy
 import v0.ui.utils as ui_utils
-import v0.ui.website as website_utils
+import v0.ui.website.utils as website_utils
 from rest_framework.response import Response
 from v0.ui.organisation.models import Organisation
 from v0.ui.common.models import BaseUser
 from v0.ui.account.models import Profile, AccountInfo
 from v0.ui.proposal.models import ProposalInfo
 from django.contrib.contenttypes.models import ContentType
+from rest_framework.views import APIView
 
 
 class GeneralUserPermissionViewSet(viewsets.ViewSet):
@@ -223,4 +224,19 @@ class RoleHierarchyViewSet(viewsets.ViewSet):
             return Response(status=200)
         except Exception as e:
             return ui_utils.handle_response(class_name, exception_object=e, request=request)
+
+
+class ManualGeneralUserPermissions(APIView):
+
+    def post(self, request):
+        class_name = self.__class__.__name__
+        permissions_list = request.data
+        for permission in permissions_list:
+            data = (GeneralUserPermission(**{
+                'name': permission['name'],
+                'is_allowed': False,
+                'profile_id': permission['profile_id'],
+            }))
+            data.save()
+        return ui_utils.handle_response(class_name, data='success', success=True)
 
