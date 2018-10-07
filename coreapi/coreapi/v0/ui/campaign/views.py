dfe6018d9f973a0a0aa626066dde93b6dd715733
Kushagra Mittal <kushagra.1986@gmail.com>
2018-10-07 18:26:11 +0000

    cache campaign leads moved to celery

--- coreapi/coreapi/v0/ui/campaign/views.py
+++ coreapi/coreapi/v0/ui/campaign/views.py
@@ -34,6 +34,7 @@ from django.conf import settings
 from v0.ui.common.models import BaseUser
 from operator import itemgetter
 import requests
+from celery import shared_task
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 from dateutil import tz
 
@@ -1748,26 +1749,31 @@ class PhaseWiseMultipleCampaignLeads(APIView):
 
         return ui_utils.handle_response(class_name, data=phase_data_all, success=True)
 
+@shared_task()
+def cache_all_campaign_leads():
+    all_leads_forms = LeadsForm.objects.all()
+    cache.clear()
+    for leads_form in all_leads_forms:
+        campaign_id = leads_form.campaign_id
+        url = "https://api.machadalo.com/v0/ui/website/dashboard/get_leads_by_campaign_new/"
+        querystring = {"campaign_id": campaign_id}
+        headers = {
+            'Authorization': "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwib3JpZ19pYXQiOjE1Mzc2OTY3NzIsIm5hbWUiOiIiLCJleHAiOjE1Mzc2OTcwNzIsInVzZXJfaWQiOjE5LCJlbWFpbCI6IiJ9.1Z8Us0_1BBWsrDGDCaJ8gLPibYmXn76sUQEo1GLXPY8",
+            'Content-Type': "application/json",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        url = "https://api.machadalo.com/v0/ui/website/dashboard/campaign_id/get_leads_by_multiple_campaigns/"
+
+        payload = "[\"" + campaign_id + "\"]"
+        response = requests.request("POST", url, data=payload, headers=headers)
+    return
+
 
 class CampaignLeadsCacheAll(APIView):
     def get(self, request):
         class_name = self.__class__.__name__
-        all_leads_forms = LeadsForm.objects.all()
-        cache.clear()
-        for leads_form in all_leads_forms:
-            campaign_id = leads_form.campaign_id
-            url = "https://api.machadalo.com/v0/ui/website/dashboard/get_leads_by_campaign_new/"
-            querystring = {"campaign_id": campaign_id}
-            headers = {
-                'Authorization': "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwib3JpZ19pYXQiOjE1Mzc2OTY3NzIsIm5hbWUiOiIiLCJleHAiOjE1Mzc2OTcwNzIsInVzZXJfaWQiOjE5LCJlbWFpbCI6IiJ9.1Z8Us0_1BBWsrDGDCaJ8gLPibYmXn76sUQEo1GLXPY8",
-                'Content-Type': "application/json",
-            }
-            response = requests.request("GET", url, headers=headers, params=querystring)
-
-            url = "https://api.machadalo.com/v0/ui/website/dashboard/campaign_id/get_leads_by_multiple_campaigns/"
-
-            payload = "[\"" + campaign_id + "\"]"
-            response = requests.request("POST", url, data=payload, headers=headers)
+        cache_all_campaign_leads.delay()
         return ui_utils.handle_response(class_name, data={"status": "success"}, success=True)
 
 
