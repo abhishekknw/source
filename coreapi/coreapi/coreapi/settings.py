125e47f6df7b802b258cc3af583badca015c5474
Kshitij Mittal <kshitij.mittal01@gmail.com>
2018-10-27 13:48:31 +0530

    minor fix

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ewis(omy!u-rgpf%9hp1^3@8ivz!upuwc&tp!0trx%#vjqs!&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Config.DEBUG if hasattr(Config, 'DEBUG') else TRUE
+DEBUG = Config.DEBUG if hasattr(Config, 'DEBUG') else True
 
 ALLOWED_HOSTS = ['13.232.210.224', 'localhost','13.127.154.33', 'api.machadalo.com', 'platform.machadalo.com',
                  '127.0.0.1']
