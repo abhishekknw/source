e694a43ad14a7ae32b5c6b8fb333761646d784c2
Kushagra <kushagra.1986@gmail.com>
2018-11-18 02:31:17 +0530

    dynamic entities edit implemented

--- coreapi/coreapi/v0/ui/dynamic_entities/views.py
+++ coreapi/coreapi/v0/ui/dynamic_entities/views.py
@@ -36,3 +36,22 @@ class EntityTypeGetOne(APIView):
             "entity_attributes": supply_entity_type.entity_attributes
         }
         return ui_utils.handle_response('', data=supply_entity_type, success=True)
+
+    @staticmethod
+    def put(request, entity_type_id):
+        new_name = request.data['name'] if 'name' in request.data else None
+        new_attributes = request.data['entity_attributes'] if 'entity_attributes' in request.data else None
+        exist_entity_query = SupplyEntityType.objects.raw({'_id':ObjectId(entity_type_id)})[0]
+        entity_name = new_name if new_name is not None else exist_entity_query.name
+        exist_attributes = exist_entity_query.entity_attributes
+        for curr_attrib in new_attributes:
+            curr_name = curr_attrib['name']
+            for i in range(0,len(exist_attributes)):
+                curr_entry = exist_attributes[i]
+                if curr_entry['name']==curr_name:
+                    exist_attributes[i] = curr_attrib
+        SupplyEntityType(**{"_id": entity_type_id,'name': entity_name, "entity_attributes": exist_attributes}).save()
+        return ui_utils.handle_response('', data="success", success=True)
+
+
+
