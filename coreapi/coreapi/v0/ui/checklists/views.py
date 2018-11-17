877e40e19a5de8d9c96d5e8642dac0d0971f512d
Kushagra <kushagra.1986@gmail.com>
2018-11-17 18:58:22 +0530

    minor change to checklist edit structure

--- coreapi/coreapi/v0/ui/checklists/views.py
+++ coreapi/coreapi/v0/ui/checklists/views.py
@@ -226,7 +226,7 @@ class ChecklistEdit(APIView):
         checklist_id = int(checklist_id)
         class_name = self.__class__.__name__
         columns = request.data['columns']
-        static_column = request.data['static_column']
+        static_column = request.data['static_columns']
         # n_rows = len(static_column)
         # n_cols = len(columns)
         # column_ids = [x["column_id"] for x in columns]
@@ -273,7 +273,6 @@ class ChecklistEdit(APIView):
                         if column in lower_level_static_column_values else None
                     mongo_client.checklist_data.update_one({'rowid': int(row_id), 'checklist_id': checklist_id}, {
                         "$set": {'data.'+column+'.lower_level_row_values': lower_level_rows}})
-                    print lower_level_rows
                 if len(row_data)>0:
                     exist_row_data = [x['data'][column] for x in checklist_data_all if x['rowid'] == row_id][0]
                     exist_row_data['cell_value'] = row_data[0]['cell_value']
