448b7d02ca5b622b71a5124d35335accc1e4236a
Kshitij Mittal <kshitij.mittal01@gmail.com>
2018-11-22 14:41:14 +0530

    POST API for Entity in Dynamic Forms

--- coreapi/coreapi/v0/ui/dynamic_entities/urls.py
+++ coreapi/coreapi/v0/ui/dynamic_entities/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
-from views import (EntityType, EntityTypeGetOne)
+from views import (EntityType, EntityTypeGetOne, Entity)
 
 urlpatterns = [
     url(r'^entity-type/$', EntityType.as_view()),
-    url(r'^entity-type/(?P<entity_type_id>[A-Z_a-z0-9]+)/$', EntityType.as_view()),
+    url(r'^entity-type/(?P<entity_type_id>[A-Z_a-z0-9]+)/$', EntityTypeGetOne.as_view()),
+    url(r'^entity/$', Entity.as_view()),
 ]
 
 router = DefaultRouter()
