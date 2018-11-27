b97b9df275a806d906731751e24f9912d6745b34
Kshitij Mittal <kshitij.mittal01@gmail.com>
2018-11-27 12:38:13 +0530

    1) validate_with_entity_type and validate_attribute_with_type methods writted to validate all the values of new entity being created against entity_type attribute types 2) is_required, options in dropdown, checkbox and radio introduced

--- coreapi/coreapi/v0/ui/dynamic_entities/views.py
+++ coreapi/coreapi/v0/ui/dynamic_entities/views.py
@@ -3,7 +3,7 @@ from v0.ui.utils import handle_response, get_user_organisation_id
 from models import SupplyEntityType, SupplyEntity
 from bson.objectid import ObjectId
 from datetime import datetime
-
+from validate_email import validate_email
 
 class EntityType(APIView):
     @staticmethod
@@ -82,6 +82,63 @@ def create_validation_msg(dict_of_required_attributes):
     return (is_valid, validation_msg_dict)
 
 
+def validate_attribute_with_type(entity_type_attribute_dict, attribute_value):
+    possible_attribute_types = ['FLOAT', 'INT', 'STRING', 'BOOLEAN', 'EMAIL', 'PASSWORD', 'PHONE', 'DROPDOWN', 'RADIO'
+                                'CHECKBOX', 'TEXTAREA', 'DATE', 'DATETIME', 'INVENTORYLIST']
+    attribute_is_valid = True
+    attribute_type = entity_type_attribute_dict['type']
+    if attribute_type in ['DROPDOWN', 'CHECKBOX', 'RADIO']:
+        attribute_options = entity_type_attribute_dict['options'] if 'options' in entity_type_attribute_dict else None
+        if not attribute_options:
+            return False
+        if attribute_value not in attribute_options:
+            return False
+    if attribute_type == 'FLOAT' and (isinstance(attribute_value,float) or isinstance(attribute_value,int)) is False:
+        return False
+    if attribute_type == 'INT' and isinstance(attribute_value,int) is False:
+        return False
+    if attribute_type == 'STRING' and isinstance(attribute_value,basestring) is False:
+        return False
+    if attribute_type == 'BOOLEAN' and isinstance(attribute_value,bool) is False:
+        return False
+    if attribute_type == 'EMAIL':
+        return validate_email(attribute_value)
+    if attribute_type == 'PASSWORD' and isinstance(attribute_value,basestring) is False:
+        return False
+    if attribute_type == 'TEXTAREA' and isinstance(attribute_value,basestring) is False:
+        return False
+    if attribute_type == 'DATE' and isinstance(attribute_value,datetime.date) is False:
+        return False
+    if attribute_type == 'DATETIME' and isinstance(attribute_value,datetime) is False:
+        return False
+    return attribute_is_valid
+
+
+def validate_with_entity_type(entity_dict,entity_type_id):
+    entity_type = list(SupplyEntityType.objects.raw({'_id': ObjectId(entity_type_id)}))
+    validation_msg_dict = {'missing_data': [], "data_mismatch": []}
+    is_valid = True
+    if len(entity_type) == 0:
+        validation_msg_dict['data_mismatch'].append("entity_type_id")
+        return (False, validation_msg_dict)
+    entity_type = entity_type[0]
+    entity_type_attribute_dict = {single_attribute["name"]: single_attribute for single_attribute in entity_type.entity_attributes}
+    new_entity_attribute_dict = {single_attribute["name"]: single_attribute for single_attribute in entity_dict["entity_attributes"]}
+    for key, value in entity_type_attribute_dict.items():
+        if 'is_required' in value and value['is_required']:
+            if key not in new_entity_attribute_dict:
+                is_valid = False
+                validation_msg_dict['missing_data'].append(key)
+        if key in new_entity_attribute_dict:
+            attribute_dict = entity_type_attribute_dict[key]
+            attribute_value = new_entity_attribute_dict[key]['value']
+            attribute_is_valid = validate_attribute_with_type(attribute_dict, attribute_value)
+            if not attribute_is_valid:
+                is_valid = False
+                validation_msg_dict['data_mismatch'].append(key)
+    return is_valid, validation_msg_dict
+
+
 class Entity(APIView):
     @staticmethod
     def post(request):
@@ -101,6 +158,9 @@ class Entity(APIView):
         entity_dict['created_at'] = datetime.now()
         if not is_valid:
             return handle_response('', data=validation_msg_dict, success=False)
+        (is_valid_adv, validation_msg_dict_adv) = validate_with_entity_type(entity_dict,entity_type_id)
+        if not is_valid_adv:
+            return handle_response('', data=validation_msg_dict_adv, success=False)
         SupplyEntity(**entity_dict).save()
         return handle_response('', data={"success": True}, success=True)
 
