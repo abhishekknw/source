f6ea3009166dd0f796e4a25b22995a48d229bd92
Kshitij Mittal <kshitij.mittal01@gmail.com>
2019-01-12 12:18:38 +0530

    timezone changed back, mysql not supporting

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -98,10 +98,10 @@ DATABASES = {
 }
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
+USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
