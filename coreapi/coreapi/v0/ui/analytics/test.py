0e03c48076a5c696e64411a6141e6d917214727d
Kushagra <kushagra.1986@gmail.com>
2019-01-17 02:59:28 +0530

    1. statistical levels split to lower and higher level, 2. Frequency distribution added - Analytics

--- coreapi/coreapi/v0/ui/analytics/test.py
+++ coreapi/coreapi/v0/ui/analytics/test.py
@@ -2,9 +2,15 @@ from __future__ import absolute_import
 import unittest
 from django.test import TestCase
 from unittest import mock
+from unittest.mock import MagicMock
+from unittest.mock import patch
 from v0.ui.analytics.views import GetLeadsDataGeneric, get_data_analytics
 
 class TestAnalytics(TestCase):
-    @mock.patch('v0.ui.analytics.views.get_data_analytics', return_value = {})
-    def test_analytics_results(self,get_data_analytics):
-        self.assertEqual(get_data_analytics([],[],[],[],[]), {})
+    #@mock.patch('v0.ui.analytics.views.get_data_analytics', return_value = {})
+    def test_get_data_analytics(self):
+        #result = get_data_analytics({},{},[],[],None)
+        self.assertEqual(get_data_analytics({},{},[],[],None), [])
+
+
+
