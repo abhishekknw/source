8632ab689a573c7346499fedbb4b3f6b417d9c84
Kshitij Mittal <kshitij.mittal01@gmail.com>
2019-02-21 11:32:23 +0530

    1) Dynamic Booking Module Added. 2) Model for BaseBookingTemplated created 3) API to create new BaseBookingTemaplte (without validation developed

---
+++ coreapi/coreapi/v0/ui/dynamic_booking/models.py
@@ -0,0 +1,20 @@
+from pymodm.connection import connect
+from pymongo.write_concern import WriteConcern
+from pymodm import MongoModel, fields
+
+connect("mongodb://localhost:27017/machadalo", alias="mongo_app")
+
+
+class BaseBookingTemplate(MongoModel):
+    name = fields.CharField()
+    base_entity_type_id = fields.CharField()
+    organisation_id = fields.CharField()
+    booking_attributes = fields.ListField()
+    entity_attributes = fields.ListField()
+    created_by = fields.CharField()
+    created_at = fields.DateTimeField()
+    updated_at = fields.DateTimeField()
+
+    class Meta:
+        write_concern = WriteConcern(j=True)
+        connection_alias = 'mongo_app'
---
+++ coreapi/coreapi/v0/ui/dynamic_booking/urls.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+from .views import (BaseBookingTemplateView)
+
+urlpatterns = [
+    url(r'^base-booking-template/$', BaseBookingTemplateView.as_view()),
+]
+
+router = DefaultRouter()
+router.include_format_suffixes = False
+urlpatterns += router.urls
