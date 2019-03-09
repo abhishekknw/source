10582af96f2a6903c58b00cce7104477304ff719
Kshitij Mittal <kshitij.mittal01@gmail.com>
2019-03-09 17:26:01 +0530

    1) Many changes and fixes in base booking template and booking template apis. 2) Postman collection updated with booking and base booking template apis properly

--- coreapi/coreapi/v0/ui/dynamic_booking/urls.py
+++ coreapi/coreapi/v0/ui/dynamic_booking/urls.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
-from .views import (BaseBookingTemplateView, BookingTemplateView, BookingTemplateTypeById)
+from .views import (BaseBookingTemplateView, BookingTemplateView, BookingTemplateById, BaseBookingTemplateById)
 
 urlpatterns = [
     url(r'^base-booking-template/$', BaseBookingTemplateView.as_view()),
+    url(r'^base-booking-template/(?P<base_booking_template_id>[A-Z_a-z0-9]+)/$', BaseBookingTemplateById.as_view()),
     url(r'^booking-template/$', BookingTemplateView.as_view()),
-    url(r'^booking-template/(?P<entity_type_id>[A-Z_a-z0-9]+)/$', BookingTemplateTypeById.as_view())
+    url(r'^booking-template/(?P<booking_template_id>[A-Z_a-z0-9]+)/$', BookingTemplateById.as_view())
 ]
 
 router = DefaultRouter()
