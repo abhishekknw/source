ed400af702f4fd541f8a36010b5d9acd655535ea
Shailesh <shailesh@mit.tc>
2019-07-08 17:42:20 +0530

    api to rmeove wrong leads

--- coreapi/coreapi/v0/ui/leads/views.py
+++ coreapi/coreapi/v0/ui/leads/views.py
@@ -706,8 +706,8 @@ class DeleteLeadEntry(APIView):
         supplier_ids = request_body.get("supplier_ids",None)
         campaign_id = request_body.get("campaign_id",None)
         for supplier_id in supplier_ids:
-            result = mongo_client.leads.delete_many({"campaign_id": campaign_id, "supplier_id": supplier_id})
-            print(result)
+            mongo_client.leads.remove({"campaign_id": campaign_id, "supplier_id": supplier_id})
+            return handle_response('', data={"success": True}, success=True)
 
 
 class DeleteLeadItem(APIView):
