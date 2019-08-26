frequency mode window calculation method changed from fixed width to fixed number of intervals

coreapi/coreapi/v0/ui/analytics/utils.py
@@ -12,7 +12,7 @@ from scipy import stats
 from v0.ui.common.models import mongo_client
 import collections
 
-
+const_intervals = 15.0
 
 def flatten(items):
     """Yield items from any nested iterable"""
@@ -38,7 +38,6 @@ def get_metrics_from_code(code, raw_metrics, derived_metrics):
 
 alternate_name_keys = {"supplier": "supplier_name", "campaign":"campaign_name"}
 alternate_name_group_dicts = {""}
-
 
 weekday_names = {'0': 'Monday', '1': 'Tuesday', '2': 'Wednesday', '3': 'Thursday',
                  '4': 'Friday', '5': 'Saturday', '6': 'Sunday'}
@@ -323,37 +322,38 @@ def calculate_freqdist_mode_from_list_floating(num_list, window_size=5):
     return freq_dist
 
 
-def calculate_freqdist_mode_from_list(num_list, window_size=5):
-    if not type(num_list) == list:
-        return {}
-    if len(num_list) == 0:
+def calculate_freqdist_mode_from_list(num_list, window_size=0, intervals = const_intervals):
+    if not type(num_list) == list or len(num_list) == 0:
         return {}
     if len(num_list) == 1:
         return [num_list, num_list[0]]
     num_list = sorted(num_list)
-    min_max = [num_list[0],num_list[-1]]
-    max = min_max[1]
-    last_window_start = max-max%window_size
+    [min_value, max_value] = [num_list[0],num_list[-1]]
+    if window_size == 0:
+        window_size = max_value/intervals
+        last_window_start = max_value - window_size
+    else:
+        last_window_start = max_value-max_value%window_size
     freq_dist = {}
     num_list_copy = num_list.copy()
     lower_limit = 0
     actual_length = len(num_list)
     counter = 0
     while lower_limit <= last_window_start:
-        upper_limit = lower_limit + window_size
+        upper_limit = round(lower_limit + window_size, 7)
+        print(lower_limit, upper_limit)
         new_list = [round_sig_min(x) for x in num_list_copy if lower_limit <= x < upper_limit]
         freq = len(new_list)
         mean = np.mean(new_list) if len(new_list)>0 else None
         counter = counter+freq
         group_name = str(lower_limit) + ' to ' + str(upper_limit)
         freq_dist[group_name] = {}
-        if new_list == []:
-            lower_limit = upper_limit
+        lower_limit = upper_limit
+        if not new_list:
             continue
         freq_dist[group_name]['values'] = new_list
         freq_dist[group_name]['mode'] = freq
         freq_dist[group_name]['mean'] = mean
-        lower_limit = upper_limit
     return freq_dist
 
 
@@ -904,19 +904,19 @@ def add_missing_keys(main_dict, main_keys):
     return main_dict
 
 
-def frequency_mode_calculator(dict_array, frequency_keys, weighted=0, window_size=5):
+def frequency_mode_calculator(dict_array, frequency_keys, weighted=0, window_size=0):
     new_array= []
     for curr_dict in dict_array:
         freq_keys = []
         for key in frequency_keys:
-            curr_dist = calculate_freqdist_mode_from_list(curr_dict[key],window_size)
+            curr_dist = calculate_freqdist_mode_from_list(curr_dict[key], window_size)
             if curr_dist == {}:
                 continue
             new_key = 'freq_dist_'+ key
             freq_keys.append(new_key)
             curr_dict[new_key] = curr_dist
         new_array.append(curr_dict)
-        x = add_missing_keys(curr_dict,freq_keys)
+        # x = add_missing_keys(curr_dict,freq_keys)
     return new_array
 
 