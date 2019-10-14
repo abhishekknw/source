4c71d30dc4ff4a49b6ac86d9476860ffde32b67d
bhawana03 <bhawana.verma@machadalo.com>
2019-10-14 14:48:15 +0530

    Adding api for getting supplier count for individual campaign status

--- coreapi/coreapi/v0/ui/ops_dashboard/urls.py
+++ coreapi/coreapi/v0/ui/ops_dashboard/urls.py
@@ -7,7 +7,8 @@ from .views import *
 urlpatterns = [
     url(r'^society-analytics/', GetSocietyAnalytics.as_view()),
     url(r'^campaign-analytics/', GetCampaignWiseAnalytics.as_view()),
-    url(r'^campaign/supplier-analytics/', GetSupplierDetail.as_view())
+    url(r'^campaign/supplier-analytics/', GetSupplierDetail.as_view()),
+    url(r'^campaign/supplier-count/', GetCampaignStatusCount.as_view())
 ]
 
 router = DefaultRouter()
