ef5711c4ff8c82791a4542b20035412e259d33bc
amit.yadav <jayesh.totla@synergytop.com>
2020-02-04 20:11:40 +0530

    Done new changes on add-suppliers-direct-to-campaign API.

--- coreapi/coreapi/v0/ui/website/utils.py
+++ coreapi/coreapi/v0/ui/website/utils.py
@@ -4022,7 +4022,7 @@ def prepare_shortlisted_spaces_and_inventories(proposal_id, page, user, assigned
             filter_query &= Q(next_action_date__gte=start_date)
             filter_query &= Q(next_action_date__lte=end_date)
         
-        shortlisted_spaces = ShortlistedSpaces.objects.filter(filter_query).order_by('id')
+        shortlisted_spaces = ShortlistedSpaces.objects.filter(filter_query).order_by('-id')
 
         if page:
             entries = 10
