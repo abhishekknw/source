8d92ae278f5d55449abfe6ffec64c23282c5cb7c
Shailesh <shailesh@mit.tc>
2020-02-26 15:12:57 +0530

    resolving migration issue for feedback in corporate

--- coreapi/coreapi/v0/ui/supplier/models.py
+++ coreapi/coreapi/v0/ui/supplier/models.py
@@ -443,7 +443,7 @@ class SupplierTypeCorporate(BasicSupplierDetails):
 
     rating = models.CharField(max_length=255, null=True, blank=True)
 
-    feedback = models.CharField(max_length=255, null=True, blank=True)
+    feedback = models.CharField(max_length=250, null=True, blank=True)
 
 
     def get_buildings(self):
