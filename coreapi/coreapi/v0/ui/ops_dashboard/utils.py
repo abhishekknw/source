99b98fd78a48fdb4c35714420781b4f208380843
bhawana03 <bhawana.verma@machadalo.com>
2020-03-02 15:49:40 +0530

    Adding internal/external comments count

--- coreapi/coreapi/v0/ui/ops_dashboard/utils.py
+++ coreapi/coreapi/v0/ui/ops_dashboard/utils.py
@@ -1,16 +1,21 @@
 import logging
 logger = logging.getLogger(__name__)
 from v0.ui.campaign.models import CampaignComments
-from v0.constants import booking_code_to_status
 
 
 def getCommentsCount(campaign_id, supplier_ids):
     try:
         if len(supplier_ids) > 0:
-            comments_count = CampaignComments.objects.filter(campaign_id=campaign_id,
-                                                             shortlisted_spaces__object_id__in=supplier_ids).values_list(
+            external_comments_count = CampaignComments.objects.filter(campaign_id=campaign_id,
+                                                             shortlisted_spaces__object_id__in=supplier_ids, related_to='EXTERNAL').values_list(
                 'shortlisted_spaces_id', flat=True).distinct().count()
-            return comments_count
+            internal_comments_count = CampaignComments.objects.filter(campaign_id=campaign_id,
+                                                             shortlisted_spaces__object_id__in=supplier_ids, related_to='INTERNAL').values_list(
+                'shortlisted_spaces_id', flat=True).distinct().count()
+            return {
+                'internal_comments_filled_count': internal_comments_count,
+                'external_comments_filled_count': external_comments_count
+            }
         else:
             return None
     except Exception as e:
@@ -18,14 +23,23 @@ def getCommentsCount(campaign_id, supplier_ids):
         return None
 
 
-def getEachCampaignComments(campaign_id, campaign_name, status_dict, supplier_dict):
-    for key in status_dict.keys():
-        comment_count = getCommentsCount(campaign_id, status_dict[key])
-        if booking_code_to_status[key] in supplier_dict:
-            supplier_dict[booking_code_to_status[key]]['comments_count'] = comment_count
-            supplier_dict[booking_code_to_status[key]]['status'] = [booking_code_to_status[key]][0].lower()
-            supplier_dict[booking_code_to_status[key]]['campaign_id'] = campaign_id
-            supplier_dict[booking_code_to_status[key]]['campaign_name'] = campaign_name
+def getEachCampaignComments(campaign_id, campaign_name, supplier_dict):
+    for key in supplier_dict.keys():
+        comments = getCommentsCount(campaign_id, supplier_dict[key]['supplier_ids'])
+        internal_comments_filled_count = external_comments_filled_count = 0
+        if comments is not None:
+            internal_comments_filled_count = comments['internal_comments_filled_count']
+            external_comments_filled_count = comments['external_comments_filled_count']
+        total_suppliers = len(supplier_dict[key]['supplier_ids'])
+        if key in supplier_dict:
+            supplier_dict[key]['internal_comments_filled_count'] = internal_comments_filled_count
+            supplier_dict[key]['external_comments_filled_count'] = external_comments_filled_count
+            supplier_dict[key]['internal_comments_not_filled_count'] = total_suppliers - internal_comments_filled_count
+            supplier_dict[key]['external_comments_not_filled_count'] = total_suppliers - external_comments_filled_count
+            supplier_dict[key]['total_suppliers'] = total_suppliers
+            supplier_dict[key]['status'] = key
+            supplier_dict[key]['campaign_id'] = campaign_id
+            supplier_dict[key]['campaign_name'] = campaign_name
     return supplier_dict
 
 
