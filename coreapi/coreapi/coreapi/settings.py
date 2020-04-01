e0a083d376cb70844ca39662d8aa78191ebdc6fc
bhawana03 <bhawana.verma@machadalo.com>
2020-04-01 15:40:07 +0530

    Adding staging ip in allowed hosts

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -35,7 +35,7 @@ if Config:
 
 ALLOWED_HOSTS = ['13.232.210.224', 'localhost','13.127.154.33', 'api.machadalo.com', 'platform.machadalo.com',
                  '127.0.0.1', 'devapi.machadalo.com','13.232.145.84', '192.168.0.113',
-                 'stagingapi.machadalo.com', 'staging.machadalo.com']
+                 'stagingapi.machadalo.com', 'staging.machadalo.com', '13.234.27.79 ']
 
 
 # Application definition
