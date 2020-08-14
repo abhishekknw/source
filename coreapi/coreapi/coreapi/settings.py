5d640ec3f4e7ee2a6b318fdd2eeec8f60a8c724e
Kushagra Mittal <kushagra.1986@gmail.com>
2020-08-14 06:32:50 +0000

    Update email id

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -207,7 +207,7 @@ else:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'businessdevelopment@machadalo.com'
-EMAIL_HOST_PASSWORD = 'Bdshapwd#126'
+EMAIL_HOST_PASSWORD = 'bdemail@clientservice#96'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 DEFAULT_EMAIL_FROM = EMAIL_HOST_USER
