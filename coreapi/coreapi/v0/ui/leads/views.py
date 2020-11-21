eed3f87f4103f83f51c3a942cfd991a1cb27f36f
Aditi Sitoke <aditi.sitoke@synergytop.com>
2020-11-21 14:38:55 +0530

    Fix key error

--- coreapi/coreapi/v0/ui/leads/views.py
+++ coreapi/coreapi/v0/ui/leads/views.py
@@ -1714,7 +1714,9 @@ def prepare_campaign_specific_data_in_excel(data, comment_list):
 
         supplier_data.append(primary_count)
         supplier_data.append(secondary_count)
-        supplier_data.append(supplier["avg_household_occupants"] if supplier['avg_household_occupants'] else None)
+        if supplier.get('avg_household_occupants'):
+            avg_household_occupants = supplier.get('avg_household_occupants')
+        supplier_data.append(avg_household_occupants)
 
         supplier_data.append(supplier['cost_per_flat'])
         supplier_data.append(booking_priority_code_to_status[supplier['booking_priority']] if supplier['booking_priority'] else None)
