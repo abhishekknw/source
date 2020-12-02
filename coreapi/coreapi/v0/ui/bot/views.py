c16031115954d7471be38ecf4fc210f48dbf0021
Aditi Sitoke <aditi.sitoke@synergytop.com>
2020-12-02 17:28:54 +0530

    Add validation in API

--- coreapi/coreapi/v0/ui/bot/views.py
+++ coreapi/coreapi/v0/ui/bot/views.py
@@ -26,27 +26,54 @@ class GetDataFromBot(APIView):
         requestId = data.get("requestId")
         datetime = data.get("datetime")
 
-        for row in data["data"]:
+        if mobile is None:
+            return ui_utils.handle_response({}, data="Phone number is Mandatory", success=False)
 
-            mongo_client.bot_requirement.insert({
-                "bot_data" : data,
-                "mobile" : mobile,
-                "sessionIds" : sessionIds,
-                "requestId" : requestId,
-                "datetime" : datetime,
-                "sector" : row.get("service"),	
-                "sub_sector" : row.get("subService"),
-                "L1Response_1": row.get("L1Response_1"),
-                "L1Response_2": row.get("L1Response_2"),
-                "L2Response_1": row.get("L2Response_1"),
-                "L2Response_2": row.get("L2Response_2"),
-                "current_partner" : row.get("existingPartner"),
-                "current_partner_feedBack" : row.get("partnerFeedback"),
-                "feedback_reason" : row.get("feedbackReason"),
-                "preferred_partner" : row.get("preferredPartner"),
-                "implementation_time"  : row.get("implementationTime"),
-                "meeting_time" : row.get("meetingTime"),
-                "call_back_time" : row.get("contactBackTime")
-            })
+        phone_number = ContactDetails.objects.filter(mobile=mobile)
+
+        if phone_number:
+            for row in data["data"]:
+                mongo_client.bot_requirement.update({
+                    "bot_data" : data,
+                    "mobile" : mobile,
+                    "sessionIds" : sessionIds,
+                    "requestId" : requestId,
+                    "datetime" : datetime,
+                    "sector" : row.get("service"),	
+                    "sub_sector" : row.get("subService"),
+                    "L1Response_1": row.get("L1Response_1"),
+                    "L1Response_2": row.get("L1Response_2"),
+                    "L2Response_1": row.get("L2Response_1"),
+                    "L2Response_2": row.get("L2Response_2"),
+                    "current_partner" : row.get("existingPartner"),
+                    "current_partner_feedBack" : row.get("partnerFeedback"),
+                    "feedback_reason" : row.get("feedbackReason"),
+                    "preferred_partner" : row.get("preferredPartner"),
+                    "implementation_time"  : row.get("implementationTime"),
+                    "meeting_time" : row.get("meetingTime"),
+                    "call_back_time" : row.get("contactBackTime")
+                })
+        else:
+            for row in data["data"]:
+                mongo_client.bot_requirement.insert({
+                    "bot_data" : data,
+                    "mobile" : mobile,
+                    "sessionIds" : sessionIds,
+                    "requestId" : requestId,
+                    "datetime" : datetime,
+                    "sector" : row.get("service"),	
+                    "sub_sector" : row.get("subService"),
+                    "L1Response_1": row.get("L1Response_1"),
+                    "L1Response_2": row.get("L1Response_2"),
+                    "L2Response_1": row.get("L2Response_1"),
+                    "L2Response_2": row.get("L2Response_2"),
+                    "current_partner" : row.get("existingPartner"),
+                    "current_partner_feedBack" : row.get("partnerFeedback"),
+                    "feedback_reason" : row.get("feedbackReason"),
+                    "preferred_partner" : row.get("preferredPartner"),
+                    "implementation_time"  : row.get("implementationTime"),
+                    "meeting_time" : row.get("meetingTime"),
+                    "call_back_time" : row.get("contactBackTime")
+                })
         
-        return ui_utils.handle_response({}, data=" Bot data successfully updated ", success=True)
+        return ui_utils.handle_response({}, data="Bot data successfully Added", success=True)
