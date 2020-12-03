75d26ae6a37bea4b4391ad151a2bf62705fe3817
Aditi Sitoke <aditi.sitoke@synergytop.com>
2020-12-03 18:53:57 +0530

    Fix lead distribution campaign list data

--- coreapi/coreapi/v0/ui/b2b/views.py
+++ coreapi/coreapi/v0/ui/b2b/views.py
@@ -1172,7 +1172,74 @@ class GetLeadDistributionCampaign(APIView):
     def get(self, request):
         
         organisation_id = request.user.profile.organisation.organisation_id
+        
+        if request.query_params.get('supplier_code') == "mix":
+            campaign_list = ProposalInfo.objects.filter(type_of_end_customer__formatted_name="b_to_b_l_d", account__organisation=organisation_id, is_mix=True).values_list('proposal_id', flat=True)
+    
+        if request.query_params.get('supplier_code') and request.query_params.get('supplier_code') != "mix" and request.query_params.get('supplier_code') != "all":
+            campaign_list = ShortlistedSpaces.objects.filter(proposal_id__type_of_end_customer__formatted_name="b_to_b_l_d", proposal_id__account__organisation=organisation_id , supplier_code=request.query_params.get('supplier_code')).values_list('proposal_id', flat=True).distinct()
+        
+        campaign_list = [campaign_id for campaign_id in campaign_list]
 
-        campaign = ProposalInfo.objects.filter(type_of_end_customer__formatted_name="b_to_b_l_d", account__organisation=organisation_id).values('proposal_id', 'name')
+        all_shortlisted_supplier = ShortlistedSpaces.objects.filter(proposal_id__in=campaign_list).\
+            values('proposal_id', 'object_id', 'is_completed', 'proposal__name', 'proposal__tentative_start_date',
+                'proposal__tentative_end_date', 'proposal__campaign_state', 'supplier_code')
 
-        return ui_utils.handle_response({}, data=campaign, success=True)
+        all_campaign_dict = {}
+        all_shortlisted_supplier_id = [supplier['object_id'] for supplier in all_shortlisted_supplier if supplier['supplier_code'] == 'RS']
+        all_supplier_society = SupplierTypeSociety.objects.filter(supplier_id__in=all_shortlisted_supplier_id).values('supplier_id', 'flat_count')
+
+        all_supplier_id = [supplier['object_id'] for supplier in all_shortlisted_supplier if supplier['supplier_code'] != 'RS']
+        all_supplier_master = SupplierMaster.objects.filter(supplier_id__in=all_supplier_id).values('supplier_id', 'unit_primary_count')
+
+        all_supplier_society_dict = {}
+        current_date = datetime.datetime.now().date()
+        for supplier in all_supplier_society:
+            all_supplier_society_dict[supplier['supplier_id']] = {'flat_count': supplier['flat_count']}
+
+        for supplier in all_supplier_master:
+            all_supplier_society_dict[supplier['supplier_id']] = {'flat_count': supplier['unit_primary_count']}
+
+        for shortlisted_supplier in all_shortlisted_supplier:
+            if shortlisted_supplier['proposal_id'] not in all_campaign_dict:
+                all_campaign_dict[shortlisted_supplier['proposal_id']] = {
+                'all_supplier_ids': [], 'all_phase_ids': [], 'total_flat_counts': 0, 'total_leads':0, 'hot_leads':0}
+            if shortlisted_supplier['object_id'] not in all_campaign_dict[shortlisted_supplier['proposal_id']]['all_supplier_ids']:
+                all_campaign_dict[shortlisted_supplier['proposal_id']]['all_supplier_ids'].append(shortlisted_supplier['object_id'])
+                if shortlisted_supplier['object_id'] in all_supplier_society_dict and all_supplier_society_dict[shortlisted_supplier['object_id']]['flat_count']:
+                    all_campaign_dict[shortlisted_supplier['proposal_id']]['total_flat_counts'] += all_supplier_society_dict[shortlisted_supplier['object_id']]['flat_count']
+
+            all_campaign_dict[shortlisted_supplier['proposal_id']]['name'] = shortlisted_supplier['proposal__name']
+            all_campaign_dict[shortlisted_supplier['proposal_id']]['start_date'] = shortlisted_supplier['proposal__tentative_start_date']
+            all_campaign_dict[shortlisted_supplier['proposal_id']]['end_date'] = shortlisted_supplier['proposal__tentative_end_date']
+            all_campaign_dict[shortlisted_supplier['proposal_id']]['campaign_status'] = shortlisted_supplier['proposal__campaign_state']
+
+        all_leads_summary = []
+        for campaign_id in all_campaign_dict:
+            this_campaign_status = None
+            if not all_campaign_dict[campaign_id]['campaign_status'] == proposal_on_hold:
+                if all_campaign_dict[campaign_id]['start_date'].date() > current_date:
+                    this_campaign_status = campaign_status['upcoming_campaigns']
+                elif all_campaign_dict[campaign_id]['end_date'].date() >= current_date:
+                    this_campaign_status = campaign_status['ongoing_campaigns']
+                elif all_campaign_dict[campaign_id]['end_date'].date() < current_date:
+                    this_campaign_status = campaign_status['completed_campaigns']
+            else:
+                this_campaign_status = "on_hold"
+            all_leads_summary.append({
+                "campaign_id": campaign_id,
+                "name": all_campaign_dict[campaign_id]['name'],
+                "start_date": all_campaign_dict[campaign_id]['start_date'],
+                "end_date": all_campaign_dict[campaign_id]['end_date'],
+                "supplier_count": len(all_campaign_dict[campaign_id]['all_supplier_ids']),
+                "flat_count": all_campaign_dict[campaign_id]['total_flat_counts'],
+                "total_leads": all_campaign_dict[campaign_id]['total_leads'],
+                "campaign_status": this_campaign_status
+            })
+        return ui_utils.handle_response({}, data=all_leads_summary, success=True)
+
+# class GetPurachsedLeadsData(APIView):
+
+#     def get(self, request):
+
+#         campaign_id = request.query_params("campaign_id")
