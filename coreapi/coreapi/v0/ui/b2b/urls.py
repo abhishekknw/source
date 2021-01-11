906673784435df1c1e696ecd5f007d417dbe6402
Rahul <rahul.rathore@synergytop.com>
2021-01-11 18:00:05 +0530

    Integrate api for count suspanse lead by date

--- coreapi/coreapi/v0/ui/b2b/urls.py
+++ coreapi/coreapi/v0/ui/b2b/urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import (BuyLead,SummaryReportAndGraph,FlatSummaryDetails,AddLeadPrice, GetLeadsByCampaignId,BdRequirement,BdVerification, ImportLead, RequirementClass, SuspenseLeadClass, BrowsedLeadClass,  
+from .views import (SuspenseLeadCount,BuyLead,SummaryReportAndGraph,FlatSummaryDetails,AddLeadPrice, GetLeadsByCampaignId,BdRequirement,BdVerification, ImportLead, RequirementClass, SuspenseLeadClass, BrowsedLeadClass,  
                         LeadOpsVerification, BrowsedToRequirement, DeleteRequirement, BrowsedLeadDelete, RestoreRequirement, GetLeadsByDate,
                         GetLeadsCampaignByDate, GetFeedbackCount, GetCampaignList, GetLeadsForCurrentCompanyDonut, GetLeadsSummeryForDonutChart,
                         GetLeadsForDonutChart,GetSupplierByCampaign, GetLeadDistributionCampaign, GetPurchasedLeadsData, GetNotPurchasedLeadsData, GetDynamicLeadFormHeaders )
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^import-lead/(?P<campaign_id>[A-Z_a-z0-9]+)/$', ImportLead.as_view()),
     url(r'^requirements/$', RequirementClass.as_view()),
     url(r'^suspance-leads/$', SuspenseLeadClass.as_view()),
+    url(r'^suspance-leads-count/$', SuspenseLeadCount.as_view()),
     url(r'^browsed-leads/$', BrowsedLeadClass.as_view()),
     url(r'^ops-lead-verification/$', LeadOpsVerification.as_view()),
     url(r'^browsed-to-requirement/$', BrowsedToRequirement.as_view()),
