ac472cb4ddfd2a83f6b9da79a410736605507362
Aditi Sitoke <aditi.sitoke@synergytop.com>
2021-03-23 18:24:42 +0530

    Update password for sending proposal file mail

--- coreapi/coreapi/coreapi/settings.py
+++ coreapi/coreapi/coreapi/settings.py
@@ -216,7 +216,7 @@ else:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'businessdevelopment@machadalo.com'
-EMAIL_HOST_PASSWORD = 'bdemail@clientservice#96'
+EMAIL_HOST_PASSWORD = 'bdemail@clientservice#2021'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 DEFAULT_EMAIL_FROM = EMAIL_HOST_USER
